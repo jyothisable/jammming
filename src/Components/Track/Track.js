0e01955e29438016206562e456818bbf6dc85c04
athul jyothis <63841734+athulJ18@users.noreply.github.com>
2021-07-20 14:06:58 +0530

    Track done

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Track.css";
+
+class Track extends React.Component {
+    renderAction() {
+        return <button>{this.props.isRemoval? '+' : '-' }</button>
+    }
+    render() {
+        return (
+                <div className="Track">
+                <div className="Track-information">
+                    <h3><!-- track name will go here --></h3>
+                    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+                </div>
+                {this.renderAction}
+                </div>
+        );
+    }
+}
+
+export default Track;
