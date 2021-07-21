155aa51e6c39be1552a746eca5503fb80a59f8e9
athul jyothis <63841734+athulJ18@users.noreply.github.com>
2021-07-22 00:00:00 +0530

    tracks hard coded done

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -4,14 +4,9 @@ import Track from "../Track/Track"
 class TrackList extends React.Component {
   render() {
     return (
-            <div className="TrackList">
-              {
-                this.props.tracks.map(track => {
-                return <Track track={track} 
-                key = {track.id} />
-                })
-              }
-            </div>
+      <div className="TrackList">
+        {this.props.tracks.map(track => <Track track={track} key={track.id} />)}
+      </div>
     );
   }
 }
