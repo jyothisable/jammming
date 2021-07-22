f75e2d7ee6fbb091d16aa5aed848c6c9f88c4619
athul jyothis <63841734+athulJ18@users.noreply.github.com>
2021-07-22 21:03:10 +0530

    playlist hardcoded

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -13,15 +13,26 @@ class App extends React.Component {
       searchResults: [
         { name: "name1", artist: "artist1", album: "album1", id: 1 },
         { name: "name2", artist: "artist2", album: "album2", id: 2 },
-        { name: "name3", artist: "artist3", album: "album3", id: 3 },
+        { name: "name3", artist: "artist3", album: "album3", id: 3 }
       ],
       playListName: "playlist",
       playListTracks: [
-        { name: "name1", artist: "artist1", album: "album1", id: 4 },
-        { name: "name2", artist: "artist2", album: "album2", id: 5 },
-        { name: "name3", artist: "artist3", album: "album3", id: 6 },],
+        { name: "pname1", artist: "artist1", album: "album1", id: 4 },
+        { name: "pname2", artist: "artist2", album: "album2", id: 5 },
+        { name: "pname3", artist: "artist3", album: "album3", id: 6 }],
     };
+    this.addTrack = this.addTrack.bind(this)
   }
+
+  addTrack(track) {
+    let tracks = this.state.playListTracks
+    if (track in tracks){
+      return
+    } 
+    tracks.push(track);
+    this.setState({playListTracks:tracks})
+  }
+
   render(){
     return (
       <div>
@@ -31,10 +42,13 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults 
+              searchResults={this.state.searchResults} 
+              onAdd = {this.addTrack}
+              />
             <Playlist
-              playListName={this.state.playlistName}
-              playListTracks={this.state.playlistTracks}
+              playListName={this.state.playListName}
+              playListTracks={this.state.playListTracks}
             />
           </div>
         </div>
