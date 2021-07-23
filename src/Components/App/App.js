82f57fe965b5474f8e130c8765f81a0860363f8c
athul jyothis <63841734+athulJ18@users.noreply.github.com>
2021-07-23 21:09:36 +0530

    playlist name change method done

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,40 +7,45 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       searchResults: [
         { name: "name1", artist: "artist1", album: "album1", id: 1 },
         { name: "name2", artist: "artist2", album: "album2", id: 2 },
-        { name: "name3", artist: "artist3", album: "album3", id: 3 }
+        { name: "name3", artist: "artist3", album: "album3", id: 3 },
       ],
-      playListName: "playlist",
+      playListName: "New Playlist",
       playListTracks: [
         { name: "pname1", artist: "artist1", album: "album1", id: 4 },
         { name: "pname2", artist: "artist2", album: "album2", id: 5 },
-        { name: "pname3", artist: "artist3", album: "album3", id: 6 }],
+        { name: "pname3", artist: "artist3", album: "album3", id: 6 },
+      ],
     };
-    this.addTrack = this.addTrack.bind(this)
-    this.removeTrack = this.removeTrack.bind(this)
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
-    let tracks = this.state.playListTracks
-    if (tracks.find(savedTrack=> savedTrack.id === track.id)){
-      return
-    } 
+    let tracks = this.state.playListTracks;
+    if (tracks.find((savedTrack) => savedTrack.id === track.id)) {
+      return;
+    }
     tracks.push(track);
-    this.setState({playListTracks:tracks})
+    this.setState({ playListTracks: tracks });
   }
 
   removeTrack(track) {
     let tracks = this.state.playListTracks;
-    tracks = tracks.filter(currentTrack=> currentTrack.id !== track.id)
+    tracks = tracks.filter((currentTrack) => currentTrack.id !== track.id);
     this.setState({ playListTracks: tracks });
   }
 
-  render(){
+  updatePlaylistName(name) {
+    this.setState({ playListName: name });
+  }
+  render() {
     return (
       <div>
         <h1>
@@ -49,14 +54,15 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults 
-              searchResults={this.state.searchResults} 
-              onAdd = {this.addTrack}
-              />
+            <SearchResults
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+            />
             <Playlist
               playListName={this.state.playListName}
               playListTracks={this.state.playListTracks}
-              onRemove = {this.removeTrack}
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
             />
           </div>
         </div>
