428fe195ed1e870765ca7a72e4c88467c44044ce
athul jyothis <63841734+athulJ18@users.noreply.github.com>
2021-07-23 23:09:18 +0530

    search bar complete

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,14 +2,32 @@ import React from 'react'
 import './SearchBar.css'
 
 class SearchBar extends React.Component {
-    render() {
-        return (
-          <div className="SearchBar">
-            <input placeholder="Enter A Song, Album, or Artist" />
-            <button className="SearchButton">SEARCH</button>
-          </div>
-        );
+  constructor(props) {
+    super(props);
+    this.state={
+      term:''
     }
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleSearchClick = this.handleSearchClick.bind(this)
+  }
+  //if we have a search field we only need to pass the value once we press search button otherwise API gets call every time we type something (no need dynamic search)
+  handleSearchClick(){
+    this.props.onSearch(this.state.term)
+  }
+  handleSearch(event) {
+    this.setState({term: event.target.value})
+  }
+  render() {
+    return (
+      <div className="SearchBar">
+        <input
+          placeholder="Enter A Song, Album, or Artist"
+          onChange={this.handleSearch}
+        />
+        <button onClick={this.handleSearchClick} className="SearchButton">SEARCH</button>
+      </div>
+    );
+  }
 }
 
 export default SearchBar
